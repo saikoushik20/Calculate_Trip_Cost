6155d13e76838bee909d3f0e6aa2331c39b77c5a
Ankarla Bhargav Raj <ankarlabhargavraj@gmail.com>
2026-01-30 15:33:03 +0530

    Extent Reports Added

--- src/test/java/testBase/baseTestClass.java
+++ src/test/java/testBase/baseTestClass.java
@@ -3,15 +3,24 @@ package testBase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.FileReader;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -24,21 +33,32 @@ public class baseTestClass {
 
 
     @BeforeSuite
-    public void setup()
-    {
+    @Parameters({"browser","os"})
+    public void setup(String browser, String os) {
         logger= LogManager.getLogger(this.getClass());
 
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            driver = new EdgeDriver(options);
+
+        } else {
+            logger.error("Browser not supported: ", browser);
+            System.out.println("Invalid browser! Supported browsers are: chrome, edge.");
+            return;
+        }
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
         configProp = new Properties();
         try (FileReader file = new FileReader(".//src//test//resources//config.properties")) {
             configProp.load(file);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        ChromeOptions options = new ChromeOptions();
-        driver=new ChromeDriver(options);
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
         driver.get(configProp.getProperty("URL"));
 
     }
@@ -48,5 +68,17 @@ public class baseTestClass {
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    public String captureScreen(String tname) throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        String targetFilePath=System.getProperty("user.dir")+"\\Screenshots\\" + tname + "_" + timeStamp + ".png";
+        File targetFile=new File(targetFilePath);
+        sourceFile.renameTo(targetFile);
+        return targetFilePath;
+
     }
 }
